0df6437ab68efd39104fd40668ec95745a52d57f
AMANULLAH023HY <amana170829@gmail.com>
2023-12-03 12:55:06 +0530

    complete signin page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -66,7 +66,7 @@ export default function SignUp() {
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
-    <h1 className='text-3xl text-center font-semibold my-7'>Signup </h1>
+    <h1 className='text-3xl text-center font-semibold my-7'>Sign Up </h1>
     <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
       <input type='text' placeholder='username' className='border p-3 rounded-lg ' onChange={handleChange} id='username' />
       <input type='email' placeholder='email' className='border p-3 rounded-lg ' onChange={handleChange} id='email' />
