5f77e5c6457b257a294050b8bf13f67802b2a957
AMANULLAH023HY <amana170829@gmail.com>
2024-01-11 23:58:48 +0530

    complete delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import  express from 'express';
-import { createList } from '../controllers/listing.controller.js';
+import { createList ,deleteListing} from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 
 router.post('/create', verifyToken,createList);
+router.delete('/delete/id', verifyToken, deleteListing)
 
 export default router;
